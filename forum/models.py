511ad88d39ac3298c405ab4201069e42390eac29
unknown <andreykryukov14642@gamil.com>
2020-07-27 21:28:03 +0300

    Изменеие Verbose_name и названия поля

--- forum/models.py
+++ forum/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Articles(models.Model):
-    create_date = models.DateTimeField(auto_now=True)
+    creation_date = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=200, verbose_name = 'Название')
     author = models.ForeignKey(
         'auth.User',
@@ -13,6 +13,6 @@ class Articles(models.Model):
         return self.title
     
     class Meta:
-        verbose_name='Статью'
+        verbose_name='Статья'
         verbose_name_plural='Статьи'
 
