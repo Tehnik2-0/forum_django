d3bd4d9471c1434d656206d288af761ca20ae1d2
unknown <andreykryukov14642@gamil.com>
2020-07-31 18:19:24 +0300

    создание разграничения доступа

--- forum/views.py
+++ forum/views.py
@@ -7,6 +7,7 @@ from django.contrib import messages
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class HomeListView(ListView):
     model = Articles
@@ -29,7 +30,8 @@ class CustomSuccessMessageMixin:
         return super().form_valid(form)
 
 
-class ArticleCreateView(CustomSuccessMessageMixin, CreateView):
+class ArticleCreateView(LoginRequiredMixin, CustomSuccessMessageMixin, CreateView, ):
+    login_url = reverse_lazy('login_page')
     model = Articles
     template_name = 'edit_page.html'
     form_class = ArticleForm
@@ -38,9 +40,15 @@ class ArticleCreateView(CustomSuccessMessageMixin, CreateView):
     def get_context_data(self, **kwargs):
         kwargs['list_articles'] = Articles.objects.all().order_by('-id')
         return super().get_context_data(**kwargs)
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.author = self.request.user
+        self.object.save()
+        return super().form_valid(form)
 
 
-class ArticleUpdateView(CustomSuccessMessageMixin, UpdateView):
+class ArticleUpdateView(CustomSuccessMessageMixin, UpdateView, LoginRequiredMixin):
+    
     model = Articles
     template_name = 'edit_page.html'
     form_class = ArticleForm
@@ -49,6 +57,11 @@ class ArticleUpdateView(CustomSuccessMessageMixin, UpdateView):
     def get_context_data(self, **kwargs):
         kwargs['update'] = True
         return super().get_context_data(**kwargs)
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        if self.request.user != kwargs['instance'].author:
+            return self.handle_no_permission()
+        return kwargs
 
 class ForumLoginView(LoginView):
     template_name = 'login.html'
@@ -76,7 +89,7 @@ class ForumLogoutView(LogoutView):
     next_page = reverse_lazy('edit_page')
 
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(DeleteView, LoginRequiredMixin):
     model = Articles
     template_name = 'edit_page.html'
     success_url = reverse_lazy('edit_page')
@@ -84,6 +97,13 @@ class ArticleDeleteView(DeleteView):
     def post(self, request, *args, **kwargs):
         messages.success(self.request, self.success_msg)
         return super().post(request)
-    
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if self.request.user != self.object.author:
+            return self.handle_no_permission()
+        success_url = self.get_success_url()
+        self.object.delete()
+        return HttpResponseRedirect(success_url)
+
 
 
