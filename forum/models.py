bb2c395af684df81604331687067ac8322f91963
unknown <andreykryukov14642@gamil.com>
2020-07-31 18:19:24 +0300

    добавление разграничения прав доступа

--- forum/models.py
+++ forum/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Articles(models.Model):
     creation_date = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=200, verbose_name = 'Название')
     author = models.ForeignKey(
-        'auth.User',
+        User,
         on_delete=models.CASCADE,
+        blank = True, 
+        null = True
     )
     body = models.TextField()
  
